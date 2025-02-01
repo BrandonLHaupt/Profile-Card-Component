09c49ab2b3790591b8e32c37b4fdb11b1a576569
Brandon Haupt <BrandonLHaupt@gmail.com>
2025-02-01 12:59:00 -0600

    asdasd

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+alert('Test')
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
